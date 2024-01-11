df16066a69f16c380f9230613a97c2a22010f7a4
Motahare <ghanbari_ustmb@yahoo.com>
2024-01-11 13:17:47 +0100

    route test

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-export function sayHallo() {
-  return "Hallo leute";
-}
+import express from "express";
+
+export const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.end("Hallo actions");
+});
+// export function sayHallo() {
+//   return "Hallo leute";
+// }
+
+app.listen(9999, () => {
+  console.log("server leuft");
+});
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,15 @@
-import { sayHallo } from "../app";
+// import { sayHallo } from "../app.js";
 
-test("Ich starte nur mein test ", () => {
-  expect(sayHallo()).toBe("Hallo leute");
+// test("Ich starte nur mein test ", () => {
+//   expect(sayHallo()).toBe("Hallo leute");
+// });
+
+import supertest from "supertest";
+import { app } from "../app.js";
+
+const request = supertest(app);
+test("Ich teste mein Route", async () => {
+  const respose = await request.get("/");
+  expect(respose.statusCode).toBe(200);
+  expect(respose.text).toBe("Hallo actions");
 });
